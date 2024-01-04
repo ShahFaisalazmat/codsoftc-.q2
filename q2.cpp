a6c9a3dbdc2d71ebd077073a033b25190e3c688c
ShahFaisalazmat <144578633+ShahFaisalazmat@users.noreply.github.com>
2024-01-04 22:23:29 +0500

    Add files via upload

---
+++ q2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std; // Add the using directive for the std namespace
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Choose operation (+, -, *, /): ";
+    cin >> operation;
+
+    double result;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << num1 << " + " << num2 << " = " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << num1 << " - " << num2 << " = " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << num1 << " / " << num2 << " = " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operation entered." << endl;
+    }
+
+    return 0;
+}
